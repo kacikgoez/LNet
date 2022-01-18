e3acec17e516183aadbc55853f0ceebf7e42db5a
kacikgoez <kawa.acikgoez@rwth-aachen.de>
2022-01-18 14:27:43 +0100

    Add GDrive Support

---
+++ Phishpedia/download.sh
@@ -0,0 +1,4 @@
+gdown --id 1nKFBogu_oMFmyBH_PvOxjH8iHRlJSLre
+gdown --id 1PLJuzxr2CUPn_uVX11QApKqhlt-HZGjO
+mv ./resnetv2_rgb_new.pth.tar ./src/siamese_pedia/resnetv2_rgb_new.pth.tar
+mv ./rcnn_bet365.pth ./src/detectron2_pedia/output/rcnn_2/rcnn_bet365.pth
