617146c8b1bf70fa1c88299f2976a9f5d42a0e85
kacikgoez <kawa.acikgoez@rwth-aachen.de>
2022-01-18 14:35:42 +0100

    Model Script Update

--- Phishpedia/download.sh
+++ Phishpedia/download.sh
@@ -1,4 +1,6 @@
 gdown --id 1nKFBogu_oMFmyBH_PvOxjH8iHRlJSLre
 gdown --id 1PLJuzxr2CUPn_uVX11QApKqhlt-HZGjO
+gdown --folder 1opJnMbyjlSx81enUCMZse5pOXalnYFk3
 mv ./resnetv2_rgb_new.pth.tar ./src/siamese_pedia/resnetv2_rgb_new.pth.tar
 mv ./rcnn_bet365.pth ./src/detectron2_pedia/output/rcnn_2/rcnn_bet365.pth
+mv ./models/ ./lnet/layoutnet/models/
