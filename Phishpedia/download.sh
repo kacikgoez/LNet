ce96b5a3fb941f27f7cc8bf137890dccc4be93df
kacikgoez <kawa.acikgoez@rwth-aachen.de>
2022-01-18 14:42:21 +0100

    Fix Download Script

--- Phishpedia/download.sh
+++ Phishpedia/download.sh
@@ -1,6 +1,6 @@
 gdown --id 1nKFBogu_oMFmyBH_PvOxjH8iHRlJSLre
 gdown --id 1PLJuzxr2CUPn_uVX11QApKqhlt-HZGjO
-gdown --folder 1opJnMbyjlSx81enUCMZse5pOXalnYFk3
+gdown --id 1u-xV9QPyq_46agUXHeGnhYV_HcrSoFmI
 mv ./resnetv2_rgb_new.pth.tar ./src/siamese_pedia/resnetv2_rgb_new.pth.tar
 mv ./rcnn_bet365.pth ./src/detectron2_pedia/output/rcnn_2/rcnn_bet365.pth
-mv ./models/ ./lnet/layoutnet/models/
+mv ./final-3d-7c-3s.pth ./lnet/layoutnet/models/final-3d-7c-3s.pth
