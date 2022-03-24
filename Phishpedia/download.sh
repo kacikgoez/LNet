c65f8b137e6e0834b13aacf96d87727d76c8a6a1
kacikgoez <74553002+kacikgoez@users.noreply.github.com>
2022-03-24 02:34:45 +0100

    Add files via upload

--- Phishpedia/download.sh
+++ Phishpedia/download.sh
@@ -1,6 +1,13 @@
 gdown --id 1nKFBogu_oMFmyBH_PvOxjH8iHRlJSLre
 gdown --id 1PLJuzxr2CUPn_uVX11QApKqhlt-HZGjO
-gdown --id 1u-xV9QPyq_46agUXHeGnhYV_HcrSoFmI
+gdown --id 11jy-ANn2fOUvHlQxaEaTsIasqSslTCh9
+gdown --id 1WVV5pZ4kmCPlUTux0x51gJIRxl_oGDjM
+gdown --id 1WSmdINK-Bwt4M5xwFzfnNjbOnHxElf1C
+gdown --id 1CDlMz9MKVTGKPVnBM5M1QJUPfnmCL7jC
+
 mv ./resnetv2_rgb_new.pth.tar ./src/siamese_pedia/resnetv2_rgb_new.pth.tar
 mv ./rcnn_bet365.pth ./src/detectron2_pedia/output/rcnn_2/rcnn_bet365.pth
-mv ./final-3d-7c-3s.pth ./lnet/layoutnet/models/final-3d-7c-3s.pth
+mv ./model-final-5x5.pth ./lnet/layoutnet/model-final-5x5.pth
+mv ./model-final-7x7.pth ./lnet/layoutnet/model-final-7x7.pth
+mv ./model_0020499.pth ./lnet/layoutnet/model_0020499.pth
+mv ./faster_rcnn.yaml ./lnet/layoutnet/faster_rcnn.yaml
